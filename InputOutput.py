27468e97cce2e3019a4ac38e6cb78d3d2cc01985
Sevynidd <sevynidd@web.de>
2022-03-08 09:02:46 +0100

    Outsource copy/write/read

--- InputOutput.py
+++ InputOutput.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sys import exc_info
 import numpy as np
 import csv
+from InputOutputFiles import Kopieren
+from InputOutputFiles import Schreiben
+from InputOutputFiles import Lesen
 
 if __name__ == '__main__':
 
@@ -10,25 +13,7 @@ if __name__ == '__main__':
         auswahl = int(input("1. Schreiben oder 2. Lesen?"))
     match auswahl:
         case 1:
-            spalten = int(input("Wie viele Spalten?"))
-            zeilen = int(input("Wie viele Zeilen?"))
-
-            arr = [[0 for x in range(spalten)] for y in range(zeilen)]
-
-            for z in range(zeilen):
-                for sp in range(spalten):
-                    if z == 0:
-                        arr[z][sp] = input(f"Überschrift {sp + 1}: ")
-                    else:
-                        arr[z][sp] = input(f"Zeile {z + 1}, Spalte {sp + 1}: ")
-            Pfad = input("In welchem Pfad soll die CSV-Datei gespeichert werden?\n(Format: "
-                         "C:\\Users\\User\\Desktop\\CSV.csv)")
-            try:
-                with open(Pfad, 'w', newline='') as file:
-                    mywriter = csv.writer(file, delimiter=';')
-                    mywriter.writerows(np.array(arr))
-            except IOError:
-                print("Es ist folgender Fehler aufgetreten: ", exc_info()[0])
+            Schreiben.func_write()
         case 2:
             Pfad = input("In welchem Pfad liegt die Csv-Datei?\n(Format: C:\\Users\\User\\Desktop\\CSV.csv)")
             try:
---
+++ InputOutputFiles/Schreiben.py
@@ -0,0 +1,23 @@
+
+
+
+def func_write():
+    spalten = int(input("Wie viele Spalten?"))
+    zeilen = int(input("Wie viele Zeilen?"))
+
+    arr = [[0 for x in range(spalten)] for y in range(zeilen)]
+
+    for z in range(zeilen):
+        for sp in range(spalten):
+            if z == 0:
+                arr[z][sp] = input(f"Überschrift {sp + 1}: ")
+            else:
+                arr[z][sp] = input(f"Zeile {z + 1}, Spalte {sp + 1}: ")
+    Pfad = input("In welchem Pfad soll die CSV-Datei gespeichert werden?\n(Format: "
+                 "C:\\Users\\User\\Desktop\\CSV.csv)")
+    try:
+        with open(Pfad, 'w', newline='') as file:
+            mywriter = csv.writer(file, delimiter=';')
+            mywriter.writerows(np.array(arr))
+    except IOError:
+        print("Es ist folgender Fehler aufgetreten: ", exc_info()[0])
