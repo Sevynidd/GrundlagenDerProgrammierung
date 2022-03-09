83e3e03ccdd474d83bd4983986bbb9101ed44abd
Sevynidd <sevynidd@web.de>
2022-03-09 21:34:04 +0100

    title, xlabel und ylabel hinzugefügt. Der Link oben folgt noch

--- WetterdatenEinlesen/WetterdatenEinlesen.py
+++ WetterdatenEinlesen/WetterdatenEinlesen.py
@@ -2,6 +2,8 @@ import csv
 import matplotlib.pyplot
 from datetime import datetime
 
+# https://www.geeksforgeeks.org/how-to-add-title-to-subplots-in-matplotlib/
+
 if __name__ == '__main__':
     dataList = []
     eingabe = 0
@@ -60,11 +62,31 @@ if __name__ == '__main__':
                             temperaturEinesJahres = 0.0
                             wertanzahl = 0
         case 3:
-            print()
+            # Öffnen der CSV Datei
+            with open('C:\\Users\\Karina\\Desktop\\07 Wetter Daten.csv', newline='') as csvfile:
+                # Zeilenweises Einlesen der CSV Datei
+                reader = csv.DictReader(csvfile)
 
+                for row in reader:
+                    timestamp_str = row['utc_timestamp']
+                    dt = datetime.fromisoformat(timestamp_str.replace('Z', '+00:00'))
+                    if dt.hour == 11:
+                        dataList.append(float(row['DE_temperature']))
 
     # Plotten der Daten
     matplotlib.pyplot.plot(dataList)
-    matplotlib.pyplot.show()
+    match eingabe:
+        case 1:
+            matplotlib.pyplot.title("1. Temperatur jedes Monats (arithmetisches Mittel)")
+            matplotlib.pyplot.xlabel("Monat")
+            matplotlib.pyplot.ylabel("Temperatur (Monatsdurchschnitt)")
+        case 2:
+            matplotlib.pyplot.title("2. Jahresdurchschnittstemperatur")
+            matplotlib.pyplot.xlabel("Jahr")
+            matplotlib.pyplot.ylabel("Temperatur (Jahresdurchschnitt)")
+        case 3:
+            matplotlib.pyplot.title("3. Temperatur immer um 11 Uhr")
+            matplotlib.pyplot.xlabel("Tag")
+            matplotlib.pyplot.ylabel("Temperatur um 11 Uhr")
 
-    print(len(dataList))
+    matplotlib.pyplot.show()
