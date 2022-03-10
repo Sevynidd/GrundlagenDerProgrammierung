ac0ca9df9f6ea3242e7a61009eaacebe1e6d2a82
Sevynidd <sevynidd@web.de>
2022-03-10 19:13:51 +0100

    Bisschen umgeschrieben und keine einzelnen Plots sondern Subplots

--- WetterdatenEinlesen/WetterdatenEinlesen.py
+++ WetterdatenEinlesen/WetterdatenEinlesen.py
@@ -1,92 +1,110 @@
 import csv
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
-# https://www.geeksforgeeks.org/how-to-add-title-to-subplots-in-matplotlib/
+dataTempJedesMonats = []
+dataJahresDurchschnitt = []
+dataTemp11Uhr = []
+
+
+def plot1():
+    temperaturEinesMonats = 0.0
+    monat = 1
+    wertanzahl = 0
+    # Öffnen der CSV Datei
+    with open('C:\\Users\\Karina\\Desktop\\07 Wetter Daten.csv', newline='') as csvfile:
+        # Zeilenweises Einlesen der CSV Datei
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            timestamp_str = row['utc_timestamp']
+            dt = datetime.fromisoformat(timestamp_str.replace('Z', '+00:00'))
+            if monat == dt.month:
+                wertanzahl += 1
+                temperaturEinesMonats += float(row['DE_temperature'])
+            else:
+                monat = dt.month
+                if not wertanzahl == 0:
+                    temperaturEinesMonats /= wertanzahl
+                    dataTempJedesMonats.append(float(temperaturEinesMonats))
+                    temperaturEinesMonats = 0.0
+                    wertanzahl = 0
+
+
+def plot2():
+    temperaturEinesJahres = 0.0
+    jahr = 1980
+    wertanzahl = 0
+    # Öffnen der CSV Datei
+    with open('C:\\Users\\Karina\\Desktop\\07 Wetter Daten.csv', newline='') as csvfile:
+        # Zeilenweises Einlesen der CSV Datei
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            timestamp_str = row['utc_timestamp']
+            dt = datetime.fromisoformat(timestamp_str.replace('Z', '+00:00'))
+            if jahr == dt.year:
+                wertanzahl += 1
+                temperaturEinesJahres += float(row['DE_temperature'])
+            else:
+                jahr += 1
+                if not wertanzahl == 0:
+                    temperaturEinesJahres /= wertanzahl
+                    dataJahresDurchschnitt.append(float(temperaturEinesJahres))
+                    temperaturEinesJahres = 0.0
+                    wertanzahl = 0
+
+
+def plot3():
+    # Öffnen der CSV Datei
+    with open('C:\\Users\\Karina\\Desktop\\07 Wetter Daten.csv', newline='') as csvfile:
+        # Zeilenweises Einlesen der CSV Datei
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            timestamp_str = row['utc_timestamp']
+            dt = datetime.fromisoformat(timestamp_str.replace('Z', '+00:00'))
+            if dt.hour == 11:
+                dataTemp11Uhr.append(float(row['DE_temperature']))
+
 
 if __name__ == '__main__':
-    dataList = []
-    eingabe = 0
 
-    while not (1 <= eingabe <= 3):
-        eingabe = int(input("1. Temperatur jedes Monats (arithmetisches Mittel)\n" +
-                            "2. Jahresdurchschnittstemperatur\n" +
-                            "3. Temperatur immer um 11 Uhr"))
+    fig, ax = plt.subplots(1, 3)
 
-    match eingabe:
-        case 1:
-            temperaturEinesMonats = 0.0
-            monat = 1
-            wertanzahl = 0
+    plot1()
 
-            # Öffnen der CSV Datei
-            with open('C:\\Users\\Karina\\Desktop\\07 Wetter Daten.csv', newline='') as csvfile:
-                # Zeilenweises Einlesen der CSV Datei
-                reader = csv.DictReader(csvfile)
+    wertanzahl = 0
 
-                for row in reader:
-                    timestamp_str = row['utc_timestamp']
-                    dt = datetime.fromisoformat(timestamp_str.replace('Z', '+00:00'))
-                    if monat == dt.month:
-                        wertanzahl += 1
-                        temperaturEinesMonats += float(row['DE_temperature'])
-                    else:
-                        monat = dt.month
-                        if not wertanzahl == 0:
-                            temperaturEinesMonats /= wertanzahl
-                            dataList.append(float(temperaturEinesMonats))
-                            temperaturEinesMonats = 0.0
-                            wertanzahl = 0
+    plot2()
 
-        case 2:
-            temperaturEinesJahres = 0.0
-            jahr = 1980
-            wertanzahl = 0
+    plot3()
 
-            # Öffnen der CSV Datei
-            with open('C:\\Users\\Karina\\Desktop\\07 Wetter Daten.csv', newline='') as csvfile:
-                # Zeilenweises Einlesen der CSV Datei
-                reader = csv.DictReader(csvfile)
+    ax[0].plot(dataTempJedesMonats)
+    ax[1].plot(dataJahresDurchschnitt)
+    ax[2].plot(dataTemp11Uhr)
 
-                for row in reader:
-                    timestamp_str = row['utc_timestamp']
-                    dt = datetime.fromisoformat(timestamp_str.replace('Z', '+00:00'))
-                    if jahr == dt.year:
-                        wertanzahl += 1
-                        temperaturEinesJahres += float(row['DE_temperature'])
-                    else:
-                        jahr += 1
-                        if not wertanzahl == 0:
-                            temperaturEinesJahres /= wertanzahl
-                            dataList.append(float(temperaturEinesJahres))
-                            temperaturEinesJahres = 0.0
-                            wertanzahl = 0
-        case 3:
-            # Öffnen der CSV Datei
-            with open('C:\\Users\\Karina\\Desktop\\07 Wetter Daten.csv', newline='') as csvfile:
-                # Zeilenweises Einlesen der CSV Datei
-                reader = csv.DictReader(csvfile)
+    ax[0].set_title("1. Temperatur jedes Monats (arithmetisches Mittel)")
+    ax[0].set_xlabel("Monat")
+    ax[0].set_ylabel("Temperatur (Monatsdurchschnitt)")
 
-                for row in reader:
-                    timestamp_str = row['utc_timestamp']
-                    dt = datetime.fromisoformat(timestamp_str.replace('Z', '+00:00'))
-                    if dt.hour == 11:
-                        dataList.append(float(row['DE_temperature']))
+    ax[1].set_title("2. Jahresdurchschnittstemperatur")
+    ax[1].set_xlabel("Jahr")
+    ax[1].set_ylabel("Temperatur (Jahresdurchschnitt)")
 
-    # Plotten der Daten
-    matplotlib.pyplot.plot(dataList)
-    match eingabe:
-        case 1:
-            matplotlib.pyplot.title("1. Temperatur jedes Monats (arithmetisches Mittel)")
-            matplotlib.pyplot.xlabel("Monat")
-            matplotlib.pyplot.ylabel("Temperatur (Monatsdurchschnitt)")
-        case 2:
-            matplotlib.pyplot.title("2. Jahresdurchschnittstemperatur")
-            matplotlib.pyplot.xlabel("Jahr")
-            matplotlib.pyplot.ylabel("Temperatur (Jahresdurchschnitt)")
-        case 3:
-            matplotlib.pyplot.title("3. Temperatur immer um 11 Uhr")
-            matplotlib.pyplot.xlabel("Tag")
-            matplotlib.pyplot.ylabel("Temperatur um 11 Uhr")
+    ax[2].set_title("3. Temperatur immer um 11 Uhr")
+    ax[2].set_xlabel("Tag")
+    ax[2].set_ylabel("Temperatur um 11 Uhr")
 
-    matplotlib.pyplot.show()
+    fig.tight_layout()
+
+    figure = plt.gcf()
+    figure.set_size_inches(14, 5, forward=True)
+    figure.set_dpi(100)
+
+    plt.get_current_fig_manager().set_window_title("Wetterdaten auslesen")
+
+    plt.show()
+
+    exit(0)
